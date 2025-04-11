51cb1bafa227cc934453bc0c69eb331afc15f8e4
felipeb.pachon@gmail.com <felipeb.pachon@gmail.com>
2025-04-11 14:25:29 -0500

    Agrega node-fetch como dependencia

--- frontend/controller/index.js
+++ frontend/controller/index.js
@@ -1,34 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('postForm').addEventListener('submit', async (event) => {
-        event.preventDefault(); // Evita que el formulario recargue la página
-    
-        const productData = {
-            product: document.getElementById('product').value,
-            price: parseFloat(document.getElementById('price').value),
-            quantity: parseInt(document.getElementById('quantity').value),
-        };
-        console.log('Datos enviados al servidor:', productData); 
-        try {
-            const response = await fetch('/.netlify/functions/post', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(productData),
-            });
-    
-            if (!response.ok) {
-                throw new Error(`Error al crear el producto: ${response.statusText}`);
-            }
-    
-            const result = await response.json();
-            alert(result.message);
-        } catch (error) {
-            console.error(error);
-            alert('Error al crear el producto. Revisa la consola para más detalles.');
-        }
-    });
-
     document.getElementById('putForm').addEventListener('submit', async (event) => {
         event.preventDefault(); // Evita que el formulario recargue la página
     
